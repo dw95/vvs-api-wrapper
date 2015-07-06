9e547230bca875ff360a0003f1db0c838395c37f
dw95 <dw95@users.noreply.github.com>
2015-07-06 22:09:07 +0200

    Added testcases

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,22 @@
 import unittest
 import datetime as dt
-from .. import VVS_EFA
+from vvs_efa import VVS_EFA
 
-vvs_efa = VVS_EFA()
+vvs_efa = VVS_EFA.VVS_EFA()
 
-class TestGetNextConnections(unittest.TestCase):
+class TestConvertNameToId(unittest.TestCase):
 
-    def test_empty_string_origin(self):
-        self.assertEqual(vvs_efa.getNextConnections("", "Feuersee", dt.datetime(2015, 7, 7, 7, 20), True), None)
+    def test_stadtbibliothek_name(self):
+        self.assertEqual(vvs_efa.convertNameToId("Stadtbibliothek"), "5006116")
 
+    def test_hauptbahnhof_name(self):
+        self.assertEqual(vvs_efa.convertNameToId("Hauptbahnhof"), "5006118")
+
+    def test_stadtbibliothek_name_mobile(self):
+        self.assertEqual(vvs_efa.convertNameToId("Stadtbibliothek", True), "5006116")
+
+    def test_hauptbahnhof_name_mobile(self):
+        self.assertEqual(vvs_efa.convertNameToId("Hauptbahnhof", True), "5006118")
 
 
 if __name__ == '__main__':
