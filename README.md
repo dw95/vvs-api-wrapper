ba2b1c0a18ebaab08dd628bc819d67bb10506d16
dw95 <dw95@users.noreply.github.com>
2015-07-09 18:12:26 +0200

    Added Stops

--- README.md
+++ README.md
@@ -22,6 +22,20 @@ for leg in connections[0].legs:
     print leg.time_of_departure.strftime("%H:%M") + " - " + leg.time_of_arrival.strftime("%H:%M") + ": "
     print leg.origin.name + " - " + leg.destination.name + " ("+ leg.line + ")" + "\n"
 
+
+charlottenplatz = VVS_EFA.Stop("Charlottenplatz")
+charlottenplatz_departures = charlottenplatz.get_next_connections(dt.datetime(2015, 7, 9, 18, 10), True)
+
+print charlottenplatz_departures[0].stop_id
+print charlottenplatz_departures[0].stop_name
+print charlottenplatz_departures[0].scheduled_datetime.strftime("%d.%m.%Y - %H:%M")
+print charlottenplatz_departures[0].realtime_datetime.strftime("%d.%m.%Y - %H:%M")
+print charlottenplatz_departures[0].serving_line.key
+print charlottenplatz_departures[0].serving_line.number
+print charlottenplatz_departures[0].serving_line.direction
+print charlottenplatz_departures[0].serving_line.direction_from
+print charlottenplatz_departures[0].serving_line.line_type
+
 ```
 
 results in:
@@ -34,4 +48,14 @@ Stuttgart, Feuersee
 07:20 - 07:22: Stadtbibliothek - Hauptbf (A.-Klett-Pl.) (U12)
 07:24 - 07:28: Hauptbf (Arnulf-Klett-Platz) - Stuttgart Hauptbahnhof (tief) ()
 07:28 - 07:31: Stuttgart Hauptbahnhof (tief) - Feuersee (S6)
+
+5006075
+Charlottenplatz
+09.07.2015 - 18:05
+09.07.2015 - 18:11
+143
+43
+Feuersee
+Killesberg
+Bus
 ```
--- VVS_EFA.py
+++ VVS_EFA.py
@@ -3,14 +3,14 @@
 import datetime as dt
 import requests
 
-class VVS_EFA:
+class VVS_EFA(object):
 
     def __init__(self):
 
         pass
 
     @classmethod
-    def convertNameToId(cls, name, mobile=False):
+    def convert_name_to_id(cls, name, mobile=False):
         if name.strip() != "": #check if string is empty -> return None
             if mobile:
                 request_url = "http://m.vvs.de/jqm/controller/XSLT_STOPFINDER_REQUEST"
@@ -65,7 +65,7 @@ class VVS_EFA:
             return None
 
 
-    def getNextConnections(self, origin, destination, datetime, departure, search_by_name = True):
+    def get_next_connections(self, origin, destination, datetime, departure, search_by_name = True):
 
         """Get connections for parameter set:
                 origin: name of origin
@@ -75,8 +75,8 @@ class VVS_EFA:
                 search_by_name: boolean --> true: search by name; false: search by id"""
 
         if search_by_name:
-            id_origin = VVS_EFA.convertNameToId(origin)
-            id_destination = VVS_EFA.convertNameToId(destination)
+            id_origin = self.convert_name_to_id(origin)
+            id_destination = self.convert_name_to_id(destination)
             if id_origin == None:
                 raise TypeError('Origin not valid or not found.')
             if id_destination == None:
@@ -200,7 +200,7 @@ class VVS_EFA:
         return trip_objs
 
 
-class Trip:
+class Trip(object):
 
     def __init__(self, origin, destination, time_of_departure, time_of_arrival, legs, fare, zones):
         """origin:            location object of origin
@@ -221,7 +221,7 @@ class Trip:
 
 
 
-class Leg:
+class Leg(object):
 
     def __init__(self, origin, destination, time_of_departure, time_of_arrival, mode, line, direction):
         """origin:            leg origin (station / address / etc.)
@@ -241,7 +241,7 @@ class Leg:
         self.direction = direction
 
 
-class Location:
+class Location(object):
 
     def __init__(self, name, loc_id, loc_type="any"):
         """type: type of location (station / address / etc.)
@@ -251,3 +251,102 @@ class Location:
         self.loc_type = loc_type
         self.name = name
         self.loc_id = loc_id
+
+
+class Stop(object):
+
+    def __init__(self, name):
+        self.id = VVS_EFA.convert_name_to_id(name)
+        if self.id == None:
+            raise TypeError("Station could not be found.")
+
+    def get_next_connections(self, datetime, departure, limit=20):
+
+        """datetime: datetime object of time & date
+           departure: boolean, true -> time of departure, false --> time of arrival
+           limit: int, limit of results fetched, default: 20"""
+
+        if self.id != None:
+
+            parameters = {}
+
+            parameters["deleteAssignedStops_dm"] = "1"
+            parameters["itOptionsActive"] = "1"
+            parameters["limit"] = str(limit)
+            parameters["locationServerActive"] = "1"
+            parameters["mode"] = "direct"
+            parameters["name_dm"] = self.id
+            parameters["ptOptionsActive"] = "1"
+            parameters["stateless"] = "1"
+            parameters["type_dm"] = "stop"
+            parameters["useAllStops"] = "1"
+            parameters["useRealtime"] = "1"
+            parameters["itdTime"] = datetime.strftime("%H%M")
+            parameters["itdDate"] = datetime.strftime("%Y%m%d")
+
+            if departure:
+                parameters["itdTripDateTimeDepArr"] = "dep"
+            else:
+                parameters["itdTripDateTimeDepArr"] = "arr"
+
+            parameters["outputFormat"] = "JSON"
+            parameters["SpEncId"] = "0"
+            parameters["anySigWhenPerfectNoOtherMatches"] = "1"
+            parameters["convertStopsPTKernel2LocationServer"] = "1"
+            parameters["convertPOIsITKernel2LocationServer"] = "1"
+            parameters["verifyAnyLocViaLocServer"] = "1"
+            parameters["w_objPrefAl"] = "12"
+            parameters["w_regPrefAm"] = "1"
+
+            request_url = "http://m.vvs.de/jqm/controller/XSLT_DM_REQUEST"
+            req = requests.post(request_url, parameters)
+            data = req.json()
+
+            departure_list = data["departureList"]
+            connections = []
+            for list_entry in departure_list:
+                serving_line_data = list_entry["servingLine"]
+                serving_line = ServingLine(serving_line_data["key"], serving_line_data["number"], serving_line_data["direction"],
+                                           serving_line_data["directionFrom"], serving_line_data["name"])
+
+                stop_id = list_entry["stopID"]
+                stop_name = list_entry["stopName"]
+                scheduled_datetime = dt.datetime(int(list_entry["dateTime"]["year"]), int(list_entry["dateTime"]["month"]),
+                                                 int(list_entry["dateTime"]["day"]), int(list_entry["dateTime"]["hour"]), int(list_entry["dateTime"]["minute"]))
+
+                try: #check if realtime info available
+                    realtime_datetime = dt.datetime(int(list_entry["realDateTime"]["year"]), int(list_entry["realDateTime"]["month"]),
+                                                    int(list_entry["realDateTime"]["day"]), int(list_entry["realDateTime"]["hour"]), int(list_entry["realDateTime"]["minute"]))
+
+                except: #if not: return None for realtime_datetime
+                    realtime_datetime = None
+
+                connection = Connection(stop_id, stop_name, scheduled_datetime, realtime_datetime, serving_line)
+                connections.append(connection)
+
+            return connections
+
+        else:
+            return None
+
+
+
+class Connection(object):
+    """docstring for Connection"""
+    def __init__(self, stop_id, stop_name, scheduled_datetime, realtime_datetime, serving_line):
+        """realtime_datetime: datetime object of projected departure/arrival. None if not available"""
+        self.stop_id = stop_id
+        self.stop_name = stop_name
+        self.scheduled_datetime = scheduled_datetime
+        self.realtime_datetime = realtime_datetime
+        self.serving_line = serving_line
+
+
+class ServingLine(object):
+    """docstring for ServingLine"""
+    def __init__(self, key, number, direction, direction_from, line_type):
+        self.key = key
+        self.number = number
+        self.direction = direction
+        self.direction_from = direction_from
+        self.line_type = line_type
